5451717e57d6c8c3bfadc1af8c7460e85bee1af0
Carlos Vieira <carlepsvieira@gmail.com>
2023-03-22 18:06:12 -0300

    refactoring server.js

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
 const app = require('./app');
-const connection = require('./db/connection');
 
 const PORT = 3001;
 
 app.listen(PORT, async () => {
   console.log(`API TrybeCash running on port ${PORT}`);
-
-  // tests connection with MySQL
-  const [result] = await connection.execute('SELECT 1');
-  if (result) {
-    console.log('MySQL connection OK');
-  }
 });
+
+// IT TESTS MYSQL CONNECTION TO EXPRESS //
+// const connection = require('./db/connection');
+// app.listen(PORT, async () => {
+//   const [result] = await connection.execute('SELECT 1');
+//   if (result) {
+//     console.log('MySQL connection OK');
+//   }
+// });
